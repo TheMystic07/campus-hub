b752066bef5c0eddcede12e2890cb301e194341c
Mystic <gushabad90@gmail.com>
2023-10-07 14:45:55 +0530

    Changed App.js , Firebase.js and installed React Router DOm

--- src/index.js
+++ src/index.js
@@ -4,6 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBkYPtfc30FneosdHZudALeaaF_rb3ncv8",
+  authDomain: "campus-hub-cu.firebaseapp.com",
+  projectId: "campus-hub-cu",
+  storageBucket: "campus-hub-cu.appspot.com",
+  messagingSenderId: "842543607008",
+  appId: "1:842543607008:web:809d0af6ace6c9b4bf3485",
+  measurementId: "G-542T39032T"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
