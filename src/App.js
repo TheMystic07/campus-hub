e3bcbacebbc506c25a4f8593b0758605189fcda5
Mystic <gushabad90@gmail.com>
2023-10-07 14:56:45 +0530

    Edited App.js - Removed Robotto Imoorts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
+import './components/LoginRegister/LoginForm'
+
 
 
 function App() {
   return (
     <div className="App">
-      
+      <LoginForm/>
     </div>
-=======
-import LoginForm from './components/LoginRegister/LoginForm';
-
-function App() {
-  return (
-   <>
-   <LoginForm/>
-   </>
->>>>>>> 388314f461e083d782aee77f5541548ee16ce5c7
   );
 }
 
